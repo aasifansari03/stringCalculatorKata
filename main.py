8dcdd30562c3b122456b1a7adb57df6ac5e210dd
Aasif Ansari <70842908+aasifansari03@users.noreply.github.com>
2022-09-07 17:28:09 +0530

    printing 0 if string is empty

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 if __name__ == "__main__":
     
     # add function
-    def add(numbers):             # takes string of comma separated elements
-        lst = numbers.split(",")  # splitting into a list
-        
-        # converting lowercase alphabets to integers
-        for i in range(len(lst)):
-            if ord(lst[i]) >= 97 and ord(lst[i]) <= 122:
-                lst[i] = ord(lst[i]) - 96  
-        
-        for i in lst:
-            print(i)
-       
-    add("5,6,2,a,b,c,d,z")
+    def add(numbers):     # takes string of comma separated elements
+        if(numbers == "" or numbers.strip() == ""):     # empty string is handled here
+            print(0)
+            
+        else:
+            lst = numbers.split(",")     # splitting into a list
+            
+            # converting alphabets to integers
+            for i in range(len(lst)):
+                if ord(lst[i])>=97 and ord(lst[i])<=122:
+                    lst[i] = ord(lst[i]) - 96
+            
+            for i in lst:
+                print(i)
+                
+    add("")
