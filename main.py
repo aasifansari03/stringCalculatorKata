607deb6eef10695b850c7fe07d81bd65a912f444
Aasif Ansari <70842908+aasifansari03@users.noreply.github.com>
2022-09-07 18:57:04 +0530

    giving an exception message for negative values
    adding the values in the string if there's no negative number

--- main.py
+++ main.py
@@ -2,18 +2,28 @@ if __name__ == "__main__":
     
     # add function
     def add(numbers):     # takes string of comma separated elements
-        if(numbers == "" or numbers.strip() == ""):     # empty string is handled here
-            print(0)
-            
+    
+        if(numbers == "" or numbers.strip() == ""):
+            print(0)   
         else:
-            lst = numbers.split(",")     # splitting into a list
+            lst = numbers.split(",")  # splitting into a list
             
             # converting alphabets to integers
             for i in range(len(lst)):
-                if ord(lst[i])>=97 and ord(lst[i])<=122:
+                if ord(lst[i][-1])>=97 and ord(lst[i][-1])<=122:     # getting the alphabet from the string
                     lst[i] = ord(lst[i]) - 96
-            
+                else:
+                    lst[i] = int(lst[i])
+                             
+            # counting negative numbers
+            negatives = []
             for i in lst:
-                print(i)
-                
-    add("")
+                if i< 0:
+                    negatives.append(i)
+
+            if(len(negatives)>0):
+                print("You entered these negative numbers : "+ str(negatives) +". Negative numbers are not allowed")
+            else:
+                print("Addition of numbers : "+ str(sum(lst)))
+                  
+    add("5,1,a,d,3,-1,-4")
