18ccdaedb83bfb8b9f4e4bfbc8a01f472b0d08fa
Aasif Ansari <70842908+aasifansari03@users.noreply.github.com>
2022-09-08 00:21:56 +0530

    throwing exception for capital letters

--- main.py
+++ main.py
@@ -1,31 +1,48 @@
 if __name__ == "__main__":
     
+
     # add function
     def add(numbers):     # takes string of comma separated elements
     
-        if(numbers == "" or numbers.strip() == ""):
+
+        if(numbers == "" or numbers.strip() == ""):     # checking if the entered string is empty or not
             print(0)   
         else:
-            numbers = numbers.replace(r"\n", ',')
+            numbers = numbers.replace(r"\n", ',')     # replacing line break with comma
             lst = numbers.split(",")     # splitting into a list
-
-            # converting alphabets to integers
-            for i in range(len(lst)):
-                if (ord(lst[i][-1]) >=97 and ord(lst[i][-1]) <= 122):     # getting the alphabet from the string
-                    lst[i] = ord(lst[i]) - 96
-                else:
-                    lst[i] = int(lst[i])
-                             
-            # counting negative numbers
-            negatives = []
+            
+            
+            # Checking for capital letters
+            capital_flag = 0
             for i in lst:
-                if i < 0:
-                    negatives.append(i)
-
-            if(len(negatives) > 0):   
-                print("You entered these negative numbers : "+ str(negatives) +". Negative numbers are not allowed")
+                if (ord(i[-1]) >= 65 and ord(i[-1]) <= 90):     # checking for capital letters
+                    capital_flag = 1
+                    break
+                else:
+                    continue
+            if(capital_flag == 1):
+                print('Sorry, capital letters not allowed. Please use lowercase letters only.')
             else:
-                print("Addition of numbers : " + str(sum([j if j <= 1000 else 0 for j in lst])))
-               
+                # converting alphabets to integers
+                for i in range(len(lst)):
+                    if (ord(lst[i][-1]) >= 97 and ord(lst[i][-1]) <= 122):     # getting the alphabet from the string
+                        lst[i] = ord(lst[i]) - 96
+                    else:
+                        lst[i] = int(lst[i])
+    
+    
+                # counting negative numbers
+                negatives = []
+                for i in lst:
+                    if i < 0:
+                        negatives.append(i)
+    
+    
+                if(len(negatives) > 0):   
+                    print("You entered these negative numbers : "+ str(negatives) +". Negative numbers are not allowed")     # printing all the negative numbers
+                else:
+                    print("Addition of numbers : " + str(sum([j if j <= 1000 else 0 for j in lst])))     # adding string only if they are equal & less than 1000
+
+
     numbersString = input('Enter numbers string: ')
     add(numbersString)
