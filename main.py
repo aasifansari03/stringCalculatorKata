9ddb2c0013e5b301be26f948dee408cd89e78bd9
Aasif Ansari <70842908+aasifansari03@users.noreply.github.com>
2022-09-07 17:16:42 +0530

    converting lowercase alphabets to integers

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 if __name__ == "__main__":
     
     # add function
-    def add(numbers): # takes string of comma separated elements
+    def add(numbers):             # takes string of comma separated elements
         lst = numbers.split(",")  # splitting into a list
+        
+        # converting lowercase alphabets to integers
+        for i in range(len(lst)):
+            if ord(lst[i]) >= 97 and ord(lst[i]) <= 122:
+                lst[i] = ord(lst[i]) - 96  
+        
         for i in lst:
             print(i)
        
-    add("5,6,2,a")
+    add("5,6,2,a,b,c,d,z")
